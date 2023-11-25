e555d6aa4941fc27f9acbf4b378f38692ce0267c
Pedro2409 <pa.cedeno@uniandes.edu.co>
2023-11-25 15:04:23 -0500

    Aprens¿dizaje mate

--- Aprendizaje Red_mate.py
+++ Aprendizaje Red_mate.py
@@ -18,7 +18,7 @@ datos = 'BD_mate.csv'
 df = pd.read_csv(datos)
 
 #modelo = BayesianNetwork([('estu_genero', 'punt_global'), ('fami_educacionmadre', 'punt_global'), ('fami_educacionpadre', 'punt_global'), ('fami_estratovivienda', 'punt_global'), ('fami_tienecomputador', 'punt_global'), ('fami_tieneinternet', 'punt_global')])
-modelo = BayesianNetwork([('periodo', 'fami_tieneinternet'), ('cole_mcpio_ubicacion', 'cole_area_ubicacion'), ('cole_mcpio_ubicacion', 'cole_naturaleza'), ('cole_mcpio_ubicacion', 'punt_matematicas'), ('cole_naturaleza', 'fami_tieneautomovil'), ('cole_naturaleza', 'punt_matematicas'), ('fami_tienecomputador', 'fami_tieneinternet'), ('fami_tienecomputador', 'fami_tieneautomovil'), ('fami_tienecomputador', 'cole_naturaleza'), ('fami_tieneinternet', 'cole_mcpio_ubicacion')])
+modelo = BayesianNetwork([('periodo', 'fami_tieneinternet'), ('periodo', 'cole_area_ubicacion'), ('periodo', 'cole_naturaleza'), ('cole_mcpio_ubicacion', 'cole_area_ubicacion'), ('cole_mcpio_ubicacion', 'punt_matematicas'), ('cole_mcpio_ubicacion', 'fami_tieneinternet'), ('cole_mcpio_ubicacion', 'fami_tienecomputador'), ('cole_naturaleza', 'fami_tieneautomovil'), ('cole_naturaleza', 'punt_matematicas'), ('cole_naturaleza', 'cole_area_ubicacion'), ('cole_naturaleza', 'fami_tieneinternet'), ('cole_naturaleza', 'cole_mcpio_ubicacion'), ('cole_naturaleza', 'fami_tienecomputador'), ('fami_tienecomputador', 'fami_tieneinternet'), ('fami_tienecomputador', 'fami_tieneautomovil'), ('fami_tienecomputador', 'estu_tipodocumento'), ('fami_tieneinternet', 'cole_area_ubicacion'), ('fami_tieneinternet', 'fami_tieneautomovil'), ('punt_matematicas', 'estu_tipodocumento'), ('punt_matematicas', 'fami_tieneautomovil'), ('punt_matematicas', 'fami_tienecomputador')])
 sample_train, sample_test = train_test_split(df, test_size=0.2, random_state=777)
 emv = MaximumLikelihoodEstimator(model = modelo, data = sample_train)
 
@@ -27,9 +27,9 @@ modelo.fit(data=sample_train, estimator=MaximumLikelihoodEstimator)
 infer = VariableElimination(modelo) 
 
 # Se extraen los valores reales
-y_real = sample_test["punt_global"].values
+y_real = sample_test["punt_matematicas"].values
 
-df2 = sample_test.drop(columns=['punt_global'])
+df2 = sample_test.drop(columns=['punt_matematicas'])
 y_p = modelo.predict(df2)
 
 accuracy = accuracy_score(y_real, y_p)
@@ -49,7 +49,7 @@ from pgmpy.estimators import K2Score
 scoring_method = K2Score(data=df)
 esth = HillClimbSearch(data=df)
 estimated_modelh = esth.estimate(
-    scoring_method=scoring_method, max_indegree=4, max_iter=int(1e1)
+    scoring_method=scoring_method, max_indegree=4, max_iter=int(1e8)
 )
 print(estimated_modelh)
 print(estimated_modelh.nodes())
@@ -61,7 +61,7 @@ from pgmpy.estimators import BicScore
 scoring_method = BicScore(data=df)
 esth = HillClimbSearch(data=df)
 estimated_modelBicScore = esth.estimate(
-    scoring_method=scoring_method, max_indegree=4, max_iter=int(1e4)
+    scoring_method=scoring_method, max_indegree=4, max_iter=int(1e5)
 )
 print(estimated_modelBicScore)
 print(estimated_modelBicScore.nodes())
