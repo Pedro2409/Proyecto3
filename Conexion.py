93eefbe01127989c7a54f7a9579a0db001f278ff
sariwis <142942705+sariwis@users.noreply.github.com>
2023-11-25 19:35:18 -0500

    al fin la conexión

--- Conexion.py
+++ Conexion.py
@@ -1,8 +1,18 @@
 import psycopg2
 engine = psycopg2.connect(
-dbname="postgres",
+dbname="p3",
 user="postgres",
 password="proyecto3",
 host="databasep3.cut51ddih3ok.us-east-1.rds.amazonaws.com",
 port='5432'
 )
+
+cursor = engine.cursor()
+
+query = """
+SELECT * 
+FROM pg_catalog.pg_tables 
+WHERE schemaname='public';"""
+cursor.execute(query)
+result = cursor.fetchall()
+result
