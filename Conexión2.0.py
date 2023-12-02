539bb80e5b3f4d2fa6b7c9a1f77a589f9c789b07
Pedro2409 <pa.cedeno@uniandes.edu.co>
2023-12-01 20:29:09 -0500

    Subir las nuevas BD

---
+++ Conexión2.0.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+psw='proyecto3'
+
+engine = psycopg2.connect(
+dbname="p3",
+user="postgres",
+password=psw,
+host="databasep3.cut51ddih3ok.us-east-1.rds.amazonaws.com",
+port='5432'
+)
+
+cursor = engine.cursor()
+
+query = """
+SELECT * 
+FROM pg_catalog.pg_tables 
+WHERE schemaname='public';"""
+cursor.execute(query)
+result = cursor.fetchall()
+result
+
+query = """
+SELECT * 
+FROM dataP3
+LIMIT 10;"""
+cursor.execute(query)
+result = cursor.fetchall()
+result
+
